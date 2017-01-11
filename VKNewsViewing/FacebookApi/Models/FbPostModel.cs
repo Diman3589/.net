d471fca293b1a3d41dbe85bb833b95dac25bcd53
DmitryZ <dmitriyzagulyaev@gmail.com>
2017-01-11 16:51:04 +0500

    модель апи фэйсбука

---
+++ VKNewsViewing/FacebookApi/Models/FbPostModel.cs
@@ -0,0 +1,17 @@
+﻿namespace FacebookApi.Models
+{
+    public struct Counter
+    {
+        public int comment_count;
+        public int share_count;
+    }
+
+    public class FbPostModel
+    {
+        public Counter share;
+        public string id;
+        public string description;
+        public string title;
+
+    }
+}
