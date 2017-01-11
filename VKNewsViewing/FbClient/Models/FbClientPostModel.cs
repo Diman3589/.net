0aee1d6ae8f6875b7f79db673a033eb220abcc63
DmitryZ <dmitriyzagulyaev@gmail.com>
2017-01-11 16:52:25 +0500

    конечная модель апи фэйсбука

---
+++ VKNewsViewing/FbClient/Models/FbClientPostModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FbClient.Models
+{
+    public class FbClientPostModel
+    {
+        public string Id;
+        public string Description;
+        public string Title;
+        public int Shares;
+        public int Comments;
+    }
+}
