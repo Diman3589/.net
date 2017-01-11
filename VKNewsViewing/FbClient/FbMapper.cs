187e97d9fed31eb7ad7d9db1570fcd2dd50ce172
DmitryZ <dmitriyzagulyaev@gmail.com>
2017-01-11 16:53:06 +0500

    конвертация моделей из апи фэйсбука в клиент фэйсбука

---
+++ VKNewsViewing/FbClient/FbMapper.cs
@@ -0,0 +1,24 @@
+﻿using FacebookApi.Models;
+using FbClient.Models;
+
+namespace FbClient
+{
+    public static class FbMapper
+    {
+        public static FbClientPostModel MapPostModel(FbPostModel model)
+        {
+            if (model == null)
+            {
+                return null;
+            }
+            return new FbClientPostModel
+            {
+                Id = model.id,
+                Title = model.title,
+                Description = model.description,
+                Shares = model.share.share_count,
+                Comments = model.share.comment_count
+            };
+        }
+    }
+}
