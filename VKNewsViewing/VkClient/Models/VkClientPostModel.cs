efb4710dfe421c0c86dca3bead81318312b2cc55
DmitryZ <dmitriyzagulyaev@gmail.com>
2017-01-11 16:59:23 +0500

    моделька

---
+++ VKNewsViewing/VkClient/Models/VkClientPostModel.cs
@@ -0,0 +1,16 @@
+﻿namespace VkClient.Models
+{
+    public class VkClientPostModel
+    {
+        public int PostId;
+        public int OwnerId;
+        public int FromId;
+        public double Date;
+        public string Text;
+        public double Likes;
+        public double Reposts;
+        public double Comments;
+        public int SourcePostId;
+        public string Attachments;
+    }
+}
---
+++ VKNewsViewing/VkClient/Models/VkClientUserModel.cs
@@ -0,0 +1,16 @@
+﻿namespace VkClient.Models
+{
+    public class VkClientUserModel
+    {
+        public int UserId;
+        public string FirstName;
+        public string LastName;
+        public string NickName;
+        public string Photo;
+        public int Hidden;
+        public bool IsMember;
+        public bool Active;
+        public int FriendsCount;
+
+    }
+}
