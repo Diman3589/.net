690dc8e106664798b459ce393955260f5bb17045
DmitryZ <dmitriyzagulyaev@gmail.com>
2017-01-11 16:57:01 +0500

    конвертация вк клиент объектов в сущности бд

---
+++ VKNewsViewing/Orm/Mapper.cs
@@ -0,0 +1,40 @@
+﻿using Domain;
+using VkClient.Models;
+
+namespace Orm
+{
+    public static class Mapper
+    {
+        public static VkUser MapVkUserModel (VkClientUserModel userModel)
+        {
+            return new VkUser
+            {
+                Id = userModel.UserId,
+                FirstName = userModel.FirstName,
+                LastName = userModel.LastName,
+                Photo = userModel.Photo,
+                Hidden = userModel.Hidden,
+                FriendsCount = userModel.FriendsCount,
+                IsMember = userModel.IsMember
+            };
+
+        }
+        public static VkPost MapVkPostModel(VkClientPostModel post)
+        {
+            return new VkPost
+            {
+                PostId = post.PostId,
+                OwnerId = post.OwnerId,
+                Text = post.Text,
+                Date = (long)post.Date,
+                RepostId = post.SourcePostId,
+                FromId = post.FromId,
+                Likes = post.Likes,
+                Comments = post.Comments,
+                Shares = post.Reposts,
+                Attachments = post.Attachments
+            };
+
+        }
+    }
+}
