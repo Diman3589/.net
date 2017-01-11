d105ff5f941d23c1bc00b226474aa3d2d7ae6c48
DmitryZ <dmitriyzagulyaev@gmail.com>
2017-01-11 16:51:57 +0500

    обработка запросов от вк апи

---
+++ VKNewsViewing/FbClient/Client.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FacebookApi;
+using FbClient.Models;
+
+namespace FbClient
+{
+    public class Client
+    {
+        private readonly FbApi _fbApi;
+
+        public Client()
+        {
+            _fbApi = new FbApi();
+        }
+
+        public async Task<List<FbClientPostModel>> GetAllFbPostsInfo(List<string> urls)
+        {
+            var allTasks = urls.Select(_fbApi.GetFbPostInfo).ToList();
+            var result = await Task.WhenAll(allTasks);
+
+            var posts = result.Select(FbMapper.MapPostModel).ToList();
+            return posts.Where(p => p != null).ToList();
+        }
+    }
+}
